7b5099be54abe8c91e66ef79b1b2b8c1dd7fbb94
Alex Ribeiro <ribeiromauroalex@gmail.com>
2022-09-27 13:11:56 -0300

    Projeto Urna Eletrônica

---
+++ etapas.js
@@ -0,0 +1,48 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38888',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'38888.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '13333',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'13333.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '38',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                fotos:[
+                    {url:'38.jpg', legenda: 'Prefeito'},
+                    {url:'38_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Zulano',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'13.jpg', legenda: 'Prefeito'},
+                    {url:'13_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
